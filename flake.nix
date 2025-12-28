a1978f62e1ddf264ebda6e89f1a998cca8bd3a85
vg006 <devvg006@gmail.com>
2025-12-28 19:21:37 +0530

    feat: Initial commit of the project

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development Environment Flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs-unstb.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, nixpkgs-unstb, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+     	let
+    		pkgs = nixpkgs.legacyPackages.${system};
+    		unstb = nixpkgs-unstb.legacyPackages.${system};
+     	in {
+        devShells.default = pkgs.mkShell {
+          NIX_SHELL_NAME="CASPER";
+          RUST_SRC_PATH="${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+         	packages = with unstb; [
+         	  # Rust
+       	    rustc
+       	    cargo
+       	    clippy
+       	    rustfmt
+       	    rust-analyzer
+
+       	    # Javascript
+       	    bun
+         	];
+        };
+      }
+    );
+}
