09026dce98bb67fc00f5ff7396b422c734adeda3
Keerthana <grittypuffy@riseup.net>
2025-12-30 19:36:53 +0530

    remove: user table for anonymous access

--- server/migrations/0001_init_database.sql
+++ server/migrations/0001_init_database.sql
@@ -1,14 +1,6 @@
--- Add migration script here
-CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    email TEXT UNIQUE,
-    password TEXT NULL,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
-);
-
 CREATE TABLE contract_packages (
     package_hash TEXT PRIMARY KEY,
-    user_id UUID NULL REFERENCES users(id) ON DELETE SET NULL,
+    user_id UUID NOT NULL,
     contract_name TEXT NOT NULL,
     owner_id TEXT NOT NULL,
     network TEXT NOT NULL,
