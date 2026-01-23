88eae844badbc49e83b8d2f077c10ae7df050888
Shalini Srinivasan <s.j.shalini2005@gmail.com>
2026-01-23 19:06:59 +0530

    [Feat]: shadcn init

--- web/src/lib/utils.ts
+++ web/src/lib/utils.ts
@@ -1,5 +1,6 @@
-export const formatHash = (hash: string, length = 12) => {
-  if (!hash) return "";
-  if (hash.length <= length) return hash;
-  return `${hash.slice(0, length / 2)}...${hash.slice(-length / 2)}`;
-};
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
